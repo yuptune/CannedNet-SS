56e0d15f6dd1f29f8934fdd19893d0b46988ab46
haze <106561594+yuptune@users.noreply.github.com>
2026-07-23 14:05:07 -0400

    revert commit because this is 2023 and not 2021 FUCK

--- CannedNet/Services/Controllers/ImageController.cs
+++ CannedNet/Services/Controllers/ImageController.cs
@@ -82,7 +82,7 @@ public class ImageController : ControllerBase
         context.Response.OnStarting(() =>
         {
             if (signature != null)
-                context.Response.Headers["Content-Signature"] = $"key-id=KEY:RSA:p1.rec.net; data=ZnVjayB5b3Ugcmo=";
+                context.Response.Headers["Content-Signature"] = $"key-id=KEY:RSA:p1.rec.net; data={signature}";
             return Task.CompletedTask;
         });
         return Results.File(imageBytes, contentType);
